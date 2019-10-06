2fc9d1c1882afc4397760b8852243bae4900ad52
https://github.com/Azim87 <you@example.com>
2019-10-06 18:45:31 +0600

    fixed error on search request

--- app/src/main/java/com/kubatov/androidthree/ui/main/MainActivity.java
+++ app/src/main/java/com/kubatov/androidthree/ui/main/MainActivity.java
@@ -1,48 +1,17 @@
 package com.kubatov.androidthree.ui.main;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.viewpager.widget.ViewPager;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.view.ViewParent;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.kubatov.androidthree.R;
-import com.kubatov.androidthree.data.model.current_weather.CurrentWeather;
-import com.kubatov.androidthree.data.network.RetroFitBuilder;
-import com.kubatov.androidthree.ui.main.forecast.ForecastActivity;
 import com.kubatov.androidthree.ui.main.viewpager.CurrentWeatherAdapter;
-import com.kubatov.androidthree.util.Toaster;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @BindView(R.id.view_pager)
     ViewPager viewPager;
-
-    @BindView(R.id.coordinator_layout)
-    CoordinatorLayout coordinatorLayout;
 
     CurrentWeatherAdapter weatherAdapter;
 
@@ -51,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initViewPager();
+    }
 
+    private void initViewPager() {
         weatherAdapter = new CurrentWeatherAdapter(getSupportFragmentManager());
         viewPager.setAdapter(weatherAdapter);
     }
