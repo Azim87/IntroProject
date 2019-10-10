dcc8da406d9a07470ff34ef0f0dd805aab99f1b8
https://github.com/Azim87 <you@example.com>
2019-10-10 13:33:27 +0600

    on button click get notification and current location

---
+++ app/src/main/java/com/kubatov/androidthree/Constants.java
@@ -0,0 +1,9 @@
+package com.kubatov.androidthree;
+
+public class Constants {
+    public static final String ICON_URL = "http://openweathermap.org/img/w/";
+
+    public static final String CITY = "Bishkek";
+
+    public static final String METRIC = "metric";
+}
